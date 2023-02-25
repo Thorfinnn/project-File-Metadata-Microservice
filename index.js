88665da316c127cf705ff2f8c7fa79149f9b20af
Taher Barwaniwala <89532500+Thorfinnn@users.noreply.github.com>
2023-02-25 12:13:01 +0000

    probably done??

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var cors = require('cors');
+var mongoose = require('mongoose');
+var multer = require('multer');
+var fs = require('fs')
+var os = require('os')
+var bodyParser = require('body-parser');
+var {GridFsStorage} = require('multer-gridfs-storage');
 require('dotenv').config()
-
+const conn = mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const storage = new GridFsStorage({db: conn});
 var app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set('json spaces',2);
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,8 +23,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+const singleUpload = multer({storage:storage}).single('upfile');
+app.post('/api/fileanalyse',singleUpload,(req,res)=>{
+  return res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size  
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
