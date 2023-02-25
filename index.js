f4a6e91c5218b221ec8b0b84f7f49915cfe13616
Taher Barwaniwala <89532500+Thorfinnn@users.noreply.github.com>
2023-02-25 12:49:37 +0000

    removed db?

--- index.js
+++ index.js
@@ -5,34 +5,43 @@ var multer = require('multer');
 var fs = require('fs')
 var os = require('os')
 var bodyParser = require('body-parser');
-var {GridFsStorage} = require('multer-gridfs-storage');
+var { GridFsStorage } = require('multer-gridfs-storage');
 require('dotenv').config()
-const conn = mongoose.connect(process.env.MONGO_URL,{
+const conn = mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-const storage = new GridFsStorage({db: conn});
+//const storage = new GridFsStorage({db: conn});
+var storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, os.tmpdir());
+    },
+    filename: function(req, file, callback){
+        callback(null, file.originalname)
+    }
+});
 var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('json spaces',2);
+app.set('json spaces', 2);
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const singleUpload = multer({storage:storage}).single('upfile');
-app.post('/api/fileanalyse',singleUpload,(req,res)=>{
+const singleUpload = multer({ storage:storage }).single('upfile');
+app.post('/api/fileanalyse', singleUpload, (req, res) => {
+  console.log(req.file.originalname);
   return res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
-    size: req.file.size  
+    size: req.file.size
   });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
